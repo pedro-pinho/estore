74b3d65d156e1c4b2e4726a534e51cba36857dc5
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2024-06-26 18:23:28 -0300

    Fetching Categories from server

--- src/app/home/services/category.service.ts
+++ src/app/home/services/category.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Category } from '../types/category.type';
-import { categories } from '../sampleData/categories.data';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
-
-  getAllCategories(): Category[] {
-    return categories;
+  getAllCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>('http://localhost:5001/categories');
   }
 }
