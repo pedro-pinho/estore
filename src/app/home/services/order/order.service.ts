6173acc4e0c2038ab623b26f1b78422129de2b72
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2024-07-01 13:40:54 -0300

    feat: Add order service and types for managing orders

---
+++ src/app/home/services/order/order.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CartStoreItem } from '../cart/cart.storeItem';
+import type { Order, OrderItem } from '../../types/order.type';
+import type { DeliveryAddress } from '../../types/cart.type';
+import { UserService } from '../users/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private httpClient: HttpClient, private cartStoreItem: CartStoreItem, private userService: UserService) {}
+
+  saveOrder(deliveryAddress: DeliveryAddress, userEmail: string): Observable<any> {
+    const url = 'http://localhost:5001/orders';
+    const orderDetails: OrderItem[] = [];
+    this.cartStoreItem.cart.products.forEach((item) => {
+      const orderItem: OrderItem = {
+        productId: item.product.id,
+        quantity: item.quantity,
+        price: item.product.price,
+        amount: item.amount,
+      };
+      orderDetails.push(orderItem);
+    });
+    const order: Order = {
+      userName: deliveryAddress.userName,
+      address: deliveryAddress.address,
+      city: deliveryAddress.city,
+      state: deliveryAddress.state,
+      pin: deliveryAddress.pin,
+      total: this.cartStoreItem.cart.totalAmount,
+      userEmail,
+      orderDetails,
+    };
+    return this.httpClient.post(url, order, {
+      headers: {
+        Authorization: this.userService.token,
+      },
+    });
+  }
+}
