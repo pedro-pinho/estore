f855415ea6fd37a1569f85bfbf72b683d2927697
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2024-07-01 10:31:03 -0300

    Added linter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsGalleryComponent } from './home/components/products-gallery/products-gallery.component';
-import { UserLoginComponent } from './home/components/users/user-login/user-login.component';
 
 export const routes: Routes = [
   {
