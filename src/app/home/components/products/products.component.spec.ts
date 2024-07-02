c7c4ea9a0c60fc1e2025676f99176c514eaff487
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2024-07-02 13:40:40 -0300

    chore: Test add to cart functionality

--- src/app/home/components/products/products.component.spec.ts
+++ src/app/home/components/products/products.component.spec.ts
@@ -5,23 +5,56 @@ import { ProductsStoreItem } from '../../services/product/products.storeItem';
 import { CartStoreItem } from '../../services/cart/cart.storeItem';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ProductStoreItemMock } from '../../../shared/mocks/product.storeItem.mock';
+import { CartStoreItemMock } from '../../../shared/mocks/cart.storeItem.mock';
+import { Product } from '../../types/product.type';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let cartStoreItem: CartStoreItem;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductsComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting(), ProductsStoreItem, CartStoreItem],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ProductsStoreItem,
+          useClass: ProductStoreItemMock,
+        },
+        {
+          provide: CartStoreItem,
+          useClass: CartStoreItemMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
+    cartStoreItem = TestBed.inject(CartStoreItem);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('addToCart', () => {
+    it('should call addProduct function in the CartStoreItem', () => {
+      const spyAddProduct: jasmine.Spy = spyOn(cartStoreItem, 'addProduct');
+      const product: Product = {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+        rating: 4,
+        description: 'Description 1',
+        image: 'image-1',
+        category_id: 1,
+      };
+      component.addToCart(product);
+      expect(spyAddProduct).toHaveBeenCalledWith(product);
+    });
   });
 });
---
+++ src/app/shared/mocks/cart.storeItem.mock.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+export class CartStoreItemMock {
+  cart$: any;
+  cart: any;
+  addProduct(): any {};
+  removeProduct: any;
+  decreaseProductQuantity: any;
+  saveCart: any;
+  clearCart: any;
+}
